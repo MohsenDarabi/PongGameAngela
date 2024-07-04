9e14ca9cc6f2a8463c2c24aeca54fea208915d51
Mohsen Darabi <38433815+MohsenDarabi@users.noreply.github.com>
2024-07-04 18:36:47 +0200

    refactor, move code to paddle class

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import turtle
 from turtle import Screen,Turtle
+
+from paddle import Paddle
+
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.color("white")
-paddle.penup()
-paddle.goto(350, 0)
-
+paddle_1 = Paddle(350,0)
+paddle_2 = Paddle(-350,0)
 def go_up():
-    paddle.goto(paddle.xcor(), paddle.ycor()+20)
+    paddle_1.goto(paddle_1.xcor(), paddle_1.ycor()+20)
 
 def go_down():
-    paddle.goto(paddle.xcor(), paddle.ycor()-20)
+    paddle_1.goto(paddle_1.xcor(), paddle_1.ycor()-20)
 screen.listen()
 screen.onkey(go_up,"Up")
 screen.onkey(go_down,"Down")
---
+++ paddle.py
@@ -0,0 +1,11 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, pos_a, pos_b):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.goto(pos_a, pos_b)
