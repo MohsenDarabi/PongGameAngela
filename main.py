956711adcb87ea9ccf272540cd2f444e2c27843e
Mohsen Darabi <38433815+MohsenDarabi@users.noreply.github.com>
2024-07-05 20:10:10 +0200

    add ball class and create move method inside it

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import time
 import turtle
 from turtle import Screen,Turtle
 
+from ball import Ball
 from paddle import Paddle
 
 screen = Screen()
@@ -11,14 +13,18 @@ screen.tracer(0)
 
 r_paddle = Paddle(350, 0)
 l_paddle = Paddle(-350,0)
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up,"Up")
 screen.onkey(r_paddle.go_down,"Down")
 
+
 game_is_on = True
 while game_is_on:
     screen.update()
+    ball.move()
+    time.sleep(0.1)
 
 
 
